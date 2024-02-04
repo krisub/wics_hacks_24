c585d2cb2955ec4588a73f35e2731454c8e877cc
Sanika Nandpure <spnandpure@gmail.com>
2024-02-03 21:30:54 -0600

    trying to get navigation to work

--- src/App.js
+++ src/App.js
@@ -1,52 +1,29 @@
-/*
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './Home';
-
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/" exact component={Home} />
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-
-export default App;
-*/
-
-
 import './App.css';
-import { BrowserRouter as Routes, Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Home/><nav>
+      <div className="App">
+        <nav>
           <ul>
             <li>
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/Login">Login</Link>
+              <Link to="/login">Login</Link>
             </li>
           </ul>
         </nav>
         <Routes>
-          <Route path="/" element ={<Home />} />
-          <Route path="/Login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
-    </div>
+      </div>
     </Router>
   );
 }
 
 export default App;
-
