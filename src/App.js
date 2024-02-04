673a9c18a4262a692f8f36866cfcc0637509d806
Sanika Nandpure <spnandpure@gmail.com>
2024-02-03 23:07:09 -0600

    signup up and running

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
+import Signup from './Signup'
 import BusinessListing from './BusinessListing';
 
 function App() {
@@ -29,12 +30,19 @@ function App() {
                 <Link to="/login"></Link>
               </li>
             )}
+
+        {false && (
+              <li color="#EBACBF">
+                <Link to="/signup"></Link>
+              </li>
+            )}
           </ul>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/businessListing" element={<BusinessListing />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
     </Router>
---
+++ src/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Signup.css'; // Import CSS file for styling
+
+function Signup(){
+    return (
+        <div>
+        <h1>signup</h1>
+      </div>
+    )
+}
+
+export default Signup;
